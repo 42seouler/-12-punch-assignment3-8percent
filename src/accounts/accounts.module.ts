44b3fa5724ac4af079b04924fa338ab85027e5ac
42seouler <77034008+42seouler@users.noreply.github.com>
2021-11-12 13:34:21 +0900

    feature : account create

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from './entities/account.entity';
+import { AccountsRepository } from './accounts.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Account, AccountsRepository])],
   controllers: [AccountsController],
-  providers: [AccountsService]
+  providers: [AccountsService],
 })
 export class AccountsModule {}
--- src/accounts/entities/account.entity.ts
+++ src/accounts/entities/account.entity.ts
@@ -1,20 +1,28 @@
 import { User } from 'src/users/entities/user.entity';
-import { Column, CreateDateColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
+@Entity()
 export class Account {
+  @PrimaryColumn()
+  accountNum: number;
 
-    @PrimaryGeneratedColumn()
-    accountNum: number;
+  @Column()
+  balance: number;
 
-    @Column()
-    balance: number;
+  @ManyToOne(() => User, (user) => user.id)
+  userId: number;
 
-    @ManyToOne(() => User, (user) => user.id)
-    userId: number;
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
 
-    @CreateDateColumn({ name: 'created_at' })
-    createdAt: Date;
-  
-    @UpdateDateColumn({ name: 'updated_at' })
-    updated_at: Date;
+  @UpdateDateColumn({ name: 'updated_at' })
+  updated_at: Date;
 }
